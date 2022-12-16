bee0a03c570dd6cd1073ebc467962b90afde1513
Kishankumary <Kishankumary@ICS-LAP-2281>
2022-12-16 16:00:36 +0530

    Add project files.

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infinite.CSharp.Day10
+{
+    class StringBuilderEx
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("hello world");
+            sb.AppendLine("hii ");
+            sb.AppendLine("Welcome to ");
+            sb.AppendLine("Infinite");
+            Console.WriteLine($"Capacity : {sb.Capacity}");
+            Console.WriteLine($"Length : {sb.Length}");
+
+            Console.WriteLine(sb.ToString());
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
